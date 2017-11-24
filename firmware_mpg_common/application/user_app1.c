8708e3434c22a48923ff71dbd363c81df24ef651
LihaoBlingBling <476677011@qq.com>
2017-11-24 12:09:40 +0800

    Fial Project X
    Fianl for my love

--- firmware_mpg_common/application/user_app1.c
+++ firmware_mpg_common/application/user_app1.c
@@ -52,8 +52,15 @@ Variable names shall start with "UserApp1_" and be declared as static.
 ***********************************************************************************************************************/
 static fnCode_type UserApp1_StateMachine;            /* The state machine function pointer */
 //static u32 UserApp1_u32Timeout;                      /* Timeout counter used across states */
-
-
+static u8 au8UserMenu1[]="************************************\n\rPress 1 to program LED command sequence\n\rPress 2 to show current USER program\n\r************************************\n\r";
+static u8 au8UserMessage1[]="\n\rEnter commands as LED-ONTIME-OFFTIME and press Enter\n\rTime is in milliseconds, max 100 commands";
+static u8 au8UserMessage2[]="\n\rLED colors: R(r),O(o),Y(y),G(g),C(c),B(b),P(p),W(w)\n\rExample: R-100-200 (RED on at 100ms and off at 200ms)\n\rPress Enter on blank line to end\n\r";
+static u8 au8UserMessage3[]="\n\rLED   ON TIME   OFF TIME\n\r-----------------------------------\n\r";
+static u8 au8Error1[]="\n\rThe Ledname is wrong\n\r";
+static u8 au8Error2[]="\n\rThe character after Ledname is wrong\n\r";
+static u8 au8Error3[]="\n\rStart time cannot include character which is not Number\n\r";
+static u8 au8Error5[]="\n\rEnd time cannot include character which is not Number\n\r";
+static u8 au8Error6[]="End time is less than Start time\n\r";
 /**********************************************************************************************************************
 Function Definitions
 **********************************************************************************************************************/
@@ -83,9 +90,14 @@ void UserApp1Initialize(void)
 {
   u8 au8UserApp1Start1[] = "LED program task started\n\r";
   
+  
   /* Turn off the Debug task command processor and announce the task is ready */
   DebugSetPassthrough();
   DebugPrintf(au8UserApp1Start1);
+  DebugLineFeed();
+  DebugPrintf(au8UserMenu1);
+
+  
   
     /* If good initialization, set state to Idle */
   if( 1 )
@@ -135,6 +147,239 @@ State Machine Function Definitions
 /* Wait for input */
 static void UserApp1SM_Idle(void)
 {
+  static LedCommandType C1;
+  static u8 u8Menu = 0;
+  static u8 u8Error = 0;
+  static u8 au8LedNames1[] = {'W','P','B','C','G','Y','O','R'};
+  static u8 au8LedNames2[] = {'w','p','b','c','g','y','o','r'};
+  static u8 au8Led[] = { WHITE,WHITE, PURPLE, BLUE, CYAN, GREEN, YELLOW, ORANGE, RED};
+  static u8 u8Number = 0;
+  static u32 u32Time1;
+  static u32 u32Time2;
+  static u8 u8Number1 = 0;
+  static u8 u8Number2 = 0;
+  static u8 u8Address1 = 1;
+  static u8 u8Address2 = 1;
+  static u32 au32Time[]={1,1,10,100,1000,10000,100000};
+  static bool bSign = FALSE;
+  static u8 u8Sign = 0;
+  static u32 u32Count = 0;
+  static u8 u8List = 0;
+  static u8 u8Count = 0;
+  static bool bInput = TRUE;
+  static bool bAgain = FALSE;
+  
+  if(u8Menu == 0)
+  {
+    if(G_au8DebugScanfBuffer[G_u8DebugScanfCharCount-1] == 0x31)
+    {
+      u8Menu=DebugScanf(G_au8DebugScanfBuffer);
+      DebugPrintf(au8UserMessage1);
+      DebugPrintf(au8UserMessage2);
+      DebugLineFeed();
+      u32Count=0;
+    }  
+    if(G_au8DebugScanfBuffer[G_u8DebugScanfCharCount-1] == 0x32)
+    {
+      u8Count=0;
+      u8Menu=DebugScanf(G_au8DebugScanfBuffer)+1;
+      DebugPrintf(au8UserMessage3);
+      DebugLineFeed();
+    }
+  }
+  if(u8Menu==1)
+  {
+    if(G_au8DebugScanfBuffer[G_u8DebugScanfCharCount-1] == 0x0D && bInput==TRUE )
+    {
+      bAgain = FALSE;
+      if(u32Count < G_u8DebugScanfCharCount)
+      {
+        if(bSign==FALSE && u8Number<=7)
+        {
+          if(G_au8DebugScanfBuffer[u32Count]==au8LedNames1[u8Number] || G_au8DebugScanfBuffer[u32Count]==au8LedNames2[u8Number])
+          {
+            bSign=TRUE;
+          }
+          u8Number++;
+        }
+        if(bSign==FALSE && u8Number>=8)
+        {
+          u8Error=1;
+        }
+        
+        if(bSign)
+        {
+          if(u8Sign == 0 && u8Error == 0)
+          {
+            u32Count++;
+            if(G_au8DebugScanfBuffer[u32Count]==0x2D)
+            {
+              u8Sign=1;
+            }
+            else
+            {
+              u8Sign=4;
+              u8Error=2;
+            }
+          }
+          
+          if(u8Sign == 1)
+          {
+            u32Count++;
+            u8Number1++;
+            if(G_au8DebugScanfBuffer[u32Count]==0x2D)
+            {
+              u8Sign=2;
+              for(u8Address1=1;u8Address1<u8Number1;u8Address1++)
+              {
+                if(G_au8DebugScanfBuffer[u32Count-u8Address1]>=48 && G_au8DebugScanfBuffer[u32Count-u8Address1]<=57)
+                {
+                  u32Time1=u32Time1+(G_au8DebugScanfBuffer[u32Count-u8Address1]-48)*au32Time[u8Address1];
+                }
+                else
+                {
+                  u8Error=3;
+                }
+              }
+            }
+          }
+          
+          if(u8Sign == 2 && u8Error==0 )
+          {
+            if(G_au8DebugScanfBuffer[u32Count]!=0x0D)
+            {
+              u32Count++;
+              u8Number2++;
+            }
+            else
+            {
+              bInput=FALSE;
+              u8Sign=3;
+              for(u8Address2=1;u8Address2<u8Number2;u8Address2++)
+              {
+                if(G_au8DebugScanfBuffer[u32Count-u8Address2]>=48 && G_au8DebugScanfBuffer[u32Count-u8Address2]<=57)
+                {
+                  u32Time2=u32Time2+(G_au8DebugScanfBuffer[u32Count-u8Address2]-48)*au32Time[u8Address2];
+                }
+                else
+                {
+                  u8Error=5;
+                }
+              }
+              DebugLineFeed();
+              if(u8Error == 0 && u32Time2<=u32Time1)
+              {
+                u8Error=6;
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    if( u8Error==0 && bInput==FALSE)
+    { 
+      if(bAgain == FALSE)
+      {
+        C1.eLED = au8Led[u8Number];
+        C1.u32Time = u32Time1;
+        C1.bOn = TRUE;
+        C1.eCurrentRate = LED_PWM_100;
+        LedDisplayAddCommand(USER_LIST , &C1);
+        
+        C1.eLED = au8Led[u8Number];
+        C1.u32Time = u32Time2;
+        C1.bOn = FALSE;
+        C1.eCurrentRate = LED_PWM_0;
+        LedDisplayAddCommand(USER_LIST , &C1);
+        bAgain=TRUE;
+        u8List++;
+      }
+      if(bAgain)
+      {
+        
+        bSign=FALSE;
+        u8Number = 0;
+        u8Number1 = 0;
+        u8Number2 = 0;
+        u8Address1 = 1;
+        u8Address2 = 1;
+        u8Sign = 0;
+        u32Time1=0;
+        u32Time2=0;
+      }
+      if(G_au8DebugScanfBuffer[G_u8DebugScanfCharCount-1] != 0x0D)
+      {
+        u32Count++;
+        bInput=TRUE;
+      }
+      if(G_au8DebugScanfBuffer[G_u8DebugScanfCharCount-1] == 0x0D &&G_au8DebugScanfBuffer[G_u8DebugScanfCharCount-2] == 0x0D )
+      {
+        u8Menu=0;
+        u8Count=DebugScanf(G_au8DebugScanfBuffer);
+      }
+    }
+    if(u8Error!=0)
+    {
+      bSign=FALSE;
+      u8Number = 0;
+      u8Number1 = 0;
+      u8Number2 = 0;
+      u8Address1 = 1;
+      u8Address2 = 1;
+      u8Sign = 0;
+      u32Time1=0;
+      u32Time2=0;
+      u32Count=G_u8DebugScanfCharCount;
+      bInput=TRUE;
+      switch(u8Error)
+      {
+      case 1 :
+        u8Error=0;
+        DebugPrintf(au8Error1);
+        break;
+      case 2 :
+        u8Error=0;
+        DebugPrintf(au8Error2);
+        break;
+      case 3 :
+        u8Error=0;
+        DebugPrintf(au8Error3);
+        break;
+      case 5 :
+        u8Error=0;
+        DebugPrintf(au8Error5);
+        break;
+      case 6 :
+        u8Error=0;
+        DebugPrintf(au8Error6);
+        break;
+      }
+      
+    }
+  }
+  if(u8Menu==2)
+  {
+    u8Menu=0;
+    for(u8Count=0;u8Count<=u8List;u8Count++)
+    {
+      LedDisplayPrintListLine(u8Count);
+    }
+    DebugPrintf(au8UserMenu1);
+    bInput=TRUE;
+    bSign=FALSE;
+    u8Number = 0;
+    u8Number1 = 0;
+    u8Number2 = 0;
+    u8Address1 = 1;
+    u8Address2 = 1;
+    u8Sign = 0;
+    u32Time1=0;
+    u32Time2=0;
+    u32Count=0;
+    
+  }
+  
   
 } /* end UserApp1SM_Idle() */
                       
